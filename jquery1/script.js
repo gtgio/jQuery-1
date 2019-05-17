70925745a26aa919763df5d742ea22d199dacbb1
Giovanni <43673511+gtgio@users.noreply.github.com>
2019-05-17 12:17:38 +0200

    Add files via upload

---
+++ jquery1/script.js
@@ -0,0 +1,25 @@
+$(document).ready( function() {
+
+	// muis events
+	$('h1').mouseenter( function() {
+		$(this).fadeTo(300, 0);
+	}).mouseleave( function() {
+		$(this).fadeTo(300, 1);
+	}).click( function() {
+		$(this).next().toggle(700);
+	});
+
+	// footer
+	$('footer').click( function() {
+		$(this).animate( {
+			top: 		'-=50px',
+			opacity: 	'-=0.2'
+		}, 900)
+	}).dblclick( function() {
+		$(this).animate( {
+			top: 		'0',
+			opacity: 	'1'
+		}, 900)
+	});
+
+});
